ddef5250251de91ecd4045b73e61dc12b68a77ef
Sachin Kulkarni <4855287+sachink2010@users.noreply.github.com>
2019-02-23 17:31:20 +0100

    Add files via upload

--- agent/agent.py
+++ agent/agent.py
@@ -1,1 +1,63 @@
 
+
+import keras
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+import numpy as np
+import random
+from collections import deque
+
+class Agent:
+    def __init__(self, state_size, is_eval=False, model_name=""):
+        self.state_size = state_size # normalized previous days
+        self.action_size = 5 #  buy_1, sell_1,DO Nothing, buy2, sell2
+        self.memory = deque(maxlen=2000)
+        self.inventory1 = []
+        self.inventory2 = []
+        self.model_name = model_name
+        self.is_eval = is_eval
+        self.gamma = 0.95 #gamma is the discount factor. It quantifies how much importance we give for future rewards.
+        self.epsilon = 1.0 #Exploration and Exploitation — Epsilon (ε)
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.model = load_model("models/" + model_name) if is_eval else self._model()
+
+    def _model(self):
+        model = Sequential()
+        model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(units=32, activation="relu"))
+        model.add(Dense(units=8, activation="relu"))
+        model.add(Dense(self.action_size, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(lr=0.0001))
+        return model
+
+    def act(self, state):
+        if not self.is_eval and random.random() <= self.epsilon:
+            #print("random action")
+            return random.randrange(self.action_size)
+        #print("Calculating using model")
+        options = self.model.predict(state)
+        #print(str(options))
+        return np.argmax(options[0])
+
+    def expReplay(self, batch_size):
+        mini_batch = []
+        l = len(self.memory)
+        
+        minibatch = random.sample(self.memory, batch_size)
+        
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+              
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay 
